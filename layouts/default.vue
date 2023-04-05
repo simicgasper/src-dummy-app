4958a368a1b7015601669f3db6067902f3d2dacb
simicgasper <simicgasper@gmail.com>
2023-04-05 20:56:09 +0200

    Added rudimentary pages

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="text-center my-3">SRC Dummy App</h1>
+      </div>
+    </div>
+
+    <ul class="nav justify-content-center">
+      <li class="nav-item">
+        <a class="nav-link" href="/users">Users</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/carts">Carts</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/products">Products</a>
+      </li>
+    </ul>
+
+    <hr>
+    
+    <Nuxt/>
+  
+  </div>
+</template>
---
+++ pages/_.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row">
+    <div class="col">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData(context) {
+    context.redirect("/users")
+  }
+}  
+</script>
